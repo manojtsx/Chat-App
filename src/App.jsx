bc5029f20f3f845b919fe2b1c31950f0f436736c
Manoj Shrestha <shresthanewar678@gmail.com>
2024-02-26 17:21:20 +0545

    initailze the project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <div className='bg-red-400'>Hello</div>
+    </>
+  )
+}
+
+export default App
