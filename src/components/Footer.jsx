68be8e201f4ba0130e798891440b57595bed9ae3
Manoj Shrestha <shresthanewar678@gmail.com>
2024-02-28 20:27:20 +0545

    completed footer design

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,20 +2,27 @@ import React from 'react';
 import FooterHeading from './mini-components/FooterHeading';
 import FooterLink from './mini-components/FooterLink';
 import FacebookRoundedIcon from '@mui/icons-material/FacebookRounded';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import InputBox from './mini-components/InputBox';
+import Button from './mini-components/Button';
 
 const Footer = () => {
+  const submitNewsletter = (event) =>{
+    event.preventDefault();
+  }
   return (
-    <div className='bg-gradient-to-r from-pink-500 to-pink-700 flex justify-around'>
+    <div className='px-4 py-8 md:px-0 bg-gradient-to-r from-pink-500 to-pink-700 flex justify-start sm:justify-around flex-wrap flex-col sm:flex-row gap-5 md:gap-0'>
         <div className='flex flex-col text-white'>
           <FooterHeading>Contact Us</FooterHeading>
           <p className='text-sm'>Chhabdi Barahi, Tanahun</p>
           <p className='text-sm'>+977-9816683613</p>
-          <p className='text-sm'>manojbicte@gmail.com</p>
+          <p className='text-sm mb-4'>manojbicte@gmail.com</p>
           <FooterHeading>Social Links</FooterHeading>
-          <div>
+          <div className='flex gap-2'>
             <FooterLink to="/"><FacebookRoundedIcon /></FooterLink>
-            <FooterLink to="/"></FooterLink>
-            <FooterLink to="/"></FooterLink>
+            <FooterLink to="/"><InstagramIcon /></FooterLink>
+            <FooterLink to="/"><GitHubIcon /></FooterLink>
           </div>
         </div>
         <div className='flex flex-col'>
@@ -30,9 +37,13 @@ const Footer = () => {
           <FooterHeading>Locate Us</FooterHeading>
           <FooterLink to="/">Sitemaps</FooterLink>
         </div>
-        <div>
+        <div className='flex flex-col gap-2'>
           <FooterHeading>Newsletter SignUp</FooterHeading>
-        </div>
+          <form className='flex flex-col gap-2' onSubmit={submitNewsletter}>
+            <InputBox type="text" name="message" placeholder="Username" required/>
+            <Button type="submit">Submit</Button>
+          </form>
+          </div>
     </div>
   )
 }
