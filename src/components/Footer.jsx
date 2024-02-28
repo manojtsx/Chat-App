d45bef4aac96577e2a26a7152fb514097bbdb7a7
Manoj Shrestha <shresthanewar678@gmail.com>
2024-02-28 20:07:38 +0545

    added footer design

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,19 +5,29 @@ import FooterLink from './mini-components/FooterLink'
 const Footer = () => {
   return (
     <div className='bg-gradient-to-r from-pink-500 to-pink-700 flex justify-around'>
-        <div>
+        <div className='flex flex-col text-white'>
           <FooterHeading>Contact Us</FooterHeading>
-          <p>Chhabdi Barahi, Tanahun</p>
-          <p>+977-9816683613</p>
-          <p>manojbicte@gmail.com</p>
+          <p className='text-sm'>Chhabdi Barahi, Tanahun</p>
+          <p className='text-sm'>+977-9816683613</p>
+          <p className='text-sm'>manojbicte@gmail.com</p>
+          <FooterHeading>Social Links</FooterHeading>
+          <div>
+            <FooterLink to="/"></FooterLink>
+            <FooterLink to="/"></FooterLink>
+            <FooterLink to="/"></FooterLink>
+          </div>
         </div>
         <div className='flex flex-col'>
           <FooterHeading>Resources</FooterHeading>
-          <FooterLink to="/login">Login</FooterLink>
-          <FooterLink to="/register">Register</FooterLink>
+          <FooterLink to="/">About</FooterLink>
+          <FooterLink to="/">Products</FooterLink>
+          <FooterLink to="/">Portfolio</FooterLink>
+          <FooterLink to="/">Contact Us</FooterLink>
+          <FooterLink to="/">Projects</FooterLink>
         </div>
-        <div>
+        <div className='flex flex-col'>
           <FooterHeading>Locate Us</FooterHeading>
+          <FooterLink to="/">Sitemaps</FooterLink>
         </div>
         <div>
           <FooterHeading>Newsletter SignUp</FooterHeading>
