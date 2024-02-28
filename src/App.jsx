eee54a4b483e02a41ef06a122d122c5ec3e772cc
Manoj Shrestha <shresthanewar678@gmail.com>
2024-02-28 19:40:08 +0545

    added Navbar and Footer component

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,20 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
       <BrowserRouter>
+      <Navbar />
       <Routes>
         <Route  path="/" element={<Home />}/>
         <Route  path="/login" element={<Login />}/>
         <Route  path="/register" element={<Register />}/>
       </Routes>
+      <Footer /> 
       </BrowserRouter>
     </>
   )
