902c9792f450ad389e40472506cecd4af31977df
Manoj Shrestha <shresthanewar678@gmail.com>
2024-02-28 19:54:14 +0545

    added footerheading component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import FooterHeading from './mini-components/FooterHeading'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='bg-gradient-to-r from-pink-500 to-pink-700 flex justify-around'>
+        <div>
+          <FooterHeading>Contact Us</FooterHeading>
+          <p>Chhabdi Barahi, Tanahun</p>
+          <p>+977-9816683613</p>
+          <p>manojbicte@gmail.com</p>
+        </div>
+        <div>
+          <FooterHeading>Resources</FooterHeading>
+          
+        </div>
+        <div>
+          <FooterHeading>Locate Us</FooterHeading>
+        </div>
+        <div>
+          <FooterHeading>Newsletter SignUp</FooterHeading>
+        </div>
+    </div>
   )
 }
 
---
+++ src/components/mini-components/FooterHeading.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FooterHeading = ({children}) => {
+  return (
+    <div className='font-semibold text-white tracking-tighter'>
+        {children}
+    </div>
+  )
+}
+
+export default FooterHeading
