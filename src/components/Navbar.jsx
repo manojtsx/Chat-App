41c61bd253b5664558c39f5dc499f4e0713616b4
Seezan Shrestha <seezanshrestha8@gmail.com>
2024-03-04 17:45:12 +0545

    Added navbar list

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,13 @@ import React from 'react'
 const Navbar = () => {
   return (
     <div className="px-4 py-4 bg-gradient-to-r from-pink-500 to-pink-700"> 
-   
+    <ul>
+      <li>Home</li>
+      <li>About</li>
+      <li>Chat</li>
+      <li>Login</li>
+      <li>Register</li>
+    </ul>
     </div>
   )
 }
