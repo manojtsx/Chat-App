98204a6f9448bc576a695776ca5fb418116d2808
Seezan Shrestha <seezanshrestha8@gmail.com>
2024-03-05 15:28:26 +0545

    added an icon and tried making the menuitem visible while clicking the icon but couldnt make it

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,25 @@
 import React from 'react'
-
+import MenuIcon from '@mui/icons-material/Menu';
 
 const Navbar = () => {
+  let Lists=[
+    {name:"Home",link:"/"},
+    {name:"About",link:"/about"},
+    {name:"Chat",link:"/chat"},
+    {name:"Login",link:"/login"},
+    {name:"Register",link:"/register"}
+  ]
+
+  const toggleMenu=()=>{
+  Lists.map((list)=>{
+    <li><a href="list.link">list.name</a></li>
+  })
+  }
   return (
-    <div className="px-4 py-4 bg-gradient-to-r from-pink-500 to-pink-700"> 
-    <ul>
-      <li>Home</li>
-      <li>About</li>
-      <li>Chat</li>
-      <li>Login</li>
-      <li>Register</li>
-    </ul>
+    <div className="px-4 py-4 fixed top-0 w-full shadow-lg bg-gradient-to-r from-pink-500 to-pink-700"> 
+    <span onClick={toggleMenu}>
+      <MenuIcon />
+    </span>
     </div>
   )
 }
