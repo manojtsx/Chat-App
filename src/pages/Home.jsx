2154999f1e976cef936de8cc55b9a5a557d6f222
Manoj Shrestha <shresthanewar678@gmail.com>
2024-03-06 20:24:21 +0545

    made the image grid in team section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import HeroSection from '../components/mini-components/HeroSection'
-import TeamSection from '../components/mini-components/TeamSection'
+import TeamSection from '../components/mini-components/reusable-components/TeamSection'
 import ContactBox from '../components/mini-components/ContactBox'
 
 const Home = () => {
