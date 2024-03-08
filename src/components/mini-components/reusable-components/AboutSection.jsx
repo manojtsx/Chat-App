aaadbbeb37bcd74eb4ce09d7d2c3efbc8bdc6512
Seezan Shrestha <seezanshrestha8@gmail.com>
2024-03-08 07:54:24 +0545

    made the responsive about page

--- src/components/mini-components/reusable-components/AboutSection.jsx
+++ src/components/mini-components/reusable-components/AboutSection.jsx
@@ -3,6 +3,7 @@ import TeamSection from './TeamSection'
 import MessageIcon from '@mui/icons-material/Message';
 import MmsIcon from '@mui/icons-material/Mms';
 import EnhancedEncryptionIcon from '@mui/icons-material/EnhancedEncryption';
+import TermsOfServices from './TermsOfServices';
 const AboutSection = () => {
     let Features=[
     {name:"Instant messaging",icon:<MessageIcon/>,description:"Engage in real-time conversations with anyone, anywhere."},
@@ -12,43 +13,41 @@ const AboutSection = () => {
   return (
     <div>
       {/* Mission */}
-        <div className=" pt-16 pb-5">
-          <div className="overflow-hidden mx-6 flex justify-center">
+        <div className=" pt-16 pb-5 sm:px-24 sm:py-20 sm:flex sm:justify-around">
+          <div className="overflow-hidden mx-6 flex justify-center sm:mx-0">
           <img src="https://www.callbell.eu/wp-content/uploads/2023/10/Slice-7-10.png" alt="Image for Chat App"  className="border rounded-3xl transition-scale duration-500 hover:scale-110"/>
           </div>
-          <p className="m-5 text-center">Chat App aims to provide a seamless and secure communication platform for users to connect, share, and collaborate with friends, family, and colleagues.</p>
+          <div className='sm:w-96 flex flex-col justify-center items-center pt-4'>
+          <h1 className='text-pink-500 font-bold text-2xl'>Chat App</h1>
+          <p className="m-5 text-center text-lg">Chat App aims to provide a seamless and secure communication platform for users to connect, share, and collaborate with friends, family, and colleagues.</p>
+          </div>
         </div>
       {/* Features */}
-        <ul className='flex flex-col px-5'>
+      <div className='flex flex-col items-center'>
+      <h1 className='text-pink-500 text-xl font-bold'>Why use Chat App?</h1>
+        <ul className='flex flex-col px-5 sm:flex-row sm:my-5'>
         {
           Features.map((feature)=>{
             return(
-              <li className="m-5 mx-10 p-10 bg-pink-200 rounded-lg shadow-lg">
+              <li className="m-5 mx-10 p-10 sm:p-16 bg-pink-200 rounded-lg shadow-lg transition-scale duration-500 hover:scale-110">
                 <div className="flex justify-center">
                   {feature.icon}
                 </div>
-                <h3 className="text-center font-bold text-pink-500">{feature.name}</h3>
+                <h3 className="text-center font-bold text-lg text-pink-500 p-3">{feature.name}</h3>
                 <p className='text-center'>{feature.description}</p>
               </li>
             )
           })
         }
         </ul>
+        </div>
       {/*Team Member*/}
-      <div>
+      <div className="mx-5 sm:mx-20">
       <TeamSection />
-      </div>
-      {/* Contact Information */}
-      <div>
-        <h3>Contact</h3>
-        <p>For any inquiries, please contact us at: <a href="mailto:seezanshrestha8@gmail.com"></a></p>
-      </div>
+      </div >
       {/* User Guidelines and Terms of serivices and privacy policy */}
       <div>
-        <h1>User Guideline</h1>
-        <p>Please adhere to our community guidelines when using our chat app. Respect other users, avoid spam, and help maintain a positive and inclusive environment for everyone.</p>
-        <h1>Terms of Service</h1>
-        <p>By using our chat app, you agree to our terms of service. Please review our terms of service before using our chat app.</p>
+        <TermsOfServices />
       </div>
     </div>
   )
---
+++ src/components/mini-components/reusable-components/TermsOfServices.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+const TermsOfServices = () => {
+  return (
+    <div className='p-10 bg-pink-50 border-2 border-pink-500 rounded m-2'>
+      <h1 className="text-pink-500 font-bold">User Guidelines</h1>
+      <ol className="list-decimal">
+        <li>Be respectful and kind to others</li>
+        <li>Do not share sensitive information</li>
+        <li>Do not engage in any illegal activities</li>
+        <li>Report it if you encounter any kind of abuse or improper content</li>
+        <li>By using Chat App, you agree to comply with these user guidelines as well as our terms of service and privacy policy.</li>
+      </ol>
+      <h1 className="text-pink-500 font-bold">Terms of Services</h1>
+      <ol className="list-decimal">
+        <li>By using Chat App, you agree to comply with our user guidelines, terms of service, and privacy policy.</li>
+        <li>Chat App is not responsible for any content shared by the users.</li>
+        <li>Chat App is not responsible for any illegal activities done by the users.</li>
+        <li>Chat App is not responsible for any sensitive information shared by the users.</li>
+        <li>Chat App is not responsible for any abuse or improper content shared by the users.</li>
+      </ol>
+      <h1 className="text-pink-500 font-bold">Privacy Policy</h1>
+      <ol className="list-decimal">
+        <li>Chat App will not share your personal information with any third party.</li>
+        <li> We take the security of your personal information seriously and employ appropriate technical and organizational measures to protect it from unauthorized access, disclosure, alteration, or destruction.</li>
+        <li>Chat App will not share your personal information with any user.</li>
+        <li>Chat App will not share your personal information with any organization.</li>
+        <li>Chat App will not share your personal information with any government agency.</li>
+        <li>Chat App will not share your personal information with any law enforcement agency.</li>
+        <li>We reserve the right to update or modify this privacy policy at any time. We will notify users of any material changes to the policy by posting the updated version on our website or within the app.</li>
+      </ol>
+    </div>
+  )
+}
+
+export default TermsOfServices
