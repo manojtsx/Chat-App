12d9d9b5d10f170c11927ab394d2f274cb399e7d
Manoj Shrestha <shresthanewar678@gmail.com>
2024-03-09 13:34:00 +0545

    created functionality to add registered data into the localStorage

--- src/components/RegisterComp.jsx
+++ src/components/RegisterComp.jsx
@@ -1,30 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import InputBox from "./mini-components/reusable-components/InputBox";
 import FormLabel from "./mini-components/reusable-components/FormLabel";
 import Button from "./mini-components/reusable-components/Button";
 import Heading from "./mini-components/reusable-components/Heading";
 
 const RegisterComp = () => {
+  const [user, setUser] = useState({
+    name : "",
+    username : "",
+    password : "",
+    nickname: ""
+    })
+
   const registerUser = (event) => {
     event.preventDefault();
+    const users = JSON.parse(localStorage.getItem("user")) || [];
+    users.push(user);
+    localStorage.setItem("user",JSON.stringify(users));
   };
+  const handleUserValue = (event) =>{
+    setUser({...user, [event.target.name] : event.target.value});
+  }
   return (
     <div className="h-screen flex justify-center items-center flex-col gap-10">
       <Heading>Registration Form - Chat App</Heading>
       <form onSubmit={registerUser} className="flex flex-col gap-5 bg-gradient-to-r from-pink-500 to-pink-700 p-5 rounded-md text-white">
         <div className="flex justify-between items-center">
           <FormLabel htmlFor="name">Name: </FormLabel>
-          <InputBox type="text" name="name" placeholder="Enter your name" required/>
+          <InputBox type="text" name="name" value={user.name} placeholder="Enter your name" onChange={handleUserValue} required/>
         </div>
         <div className="flex justify-between items-center">
           <FormLabel htmlFor="username">Username: </FormLabel>
-        <InputBox type="text" name="username" placeholder="Enter your username" required/>
+        <InputBox type="text" name="username" value={user.username} placeholder="Enter your username" onChange={handleUserValue} required/>
         </div>
         <div className="flex justify-between items-center">
         <FormLabel htmlFor="password">Password: </FormLabel>
-        <InputBox type="password" name="password" placeholder="Enter your password" required/>
+        <InputBox type="password" name="password" value={user.password} placeholder="Enter your password" onChange={handleUserValue} required/>
         </div>
-        <Button type="submit">Register</Button>
+        <Button type="submit" on>Register</Button>
       </form>
     </div>
   );
