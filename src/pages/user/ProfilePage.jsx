637a66c5066d4f0007d741998498d52f35ea3709
Manoj Shrestha <shresthanewar678@gmail.com>
2024-03-13 19:46:01 +0545

    created profile page

--- src/pages/user/ProfilePage.jsx
+++ src/pages/user/ProfilePage.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
+import { useParams, useNavigate } from "react-router-dom";
 
 const ProfilePage = () => {
+  const navigate = useNavigate();
+  const { id } = useParams();
+  const allUserData = JSON.parse(localStorage.getItem("user"));
+  const rightUserData = allUserData.find((user) => {
+    return user.id === Number(id);
+  });
   return (
-    <div>Invalid URL</div>
-  )
-}
+    <div className="flex justify-center items-center h-screen">
+      <div className="bg-gradient-to-r from-pink-500 to-pink-700 text-gray-200">
+        <h1 className="text-3xl">Your Profile</h1>
+        <div className="self-center">
+          <p>Name : {rightUserData.name}</p>
+          <p>Password : {rightUserData.password}</p>
+          <p>Nickname : {rightUserData.nickname? rightUserData.nickname : "No nickname"}</p>
+          <button onClick={()=>navigate(`/profile/edit/${rightUserData.id}`)}>Edit</button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default ProfilePage
+export default ProfilePage;
